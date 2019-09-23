e28d5865f8fab531de606b6e9d381ef540318325
amannirala13 <asdevofficial@gmail.com>
2019-09-24 03:25:44 +0530

    Updated facebook automation script

--- Automation/Web/facebook_automation.py
+++ Automation/Web/facebook_automation.py
@@ -1,16 +1,19 @@
-from selenium import webdriver
+#Automation of facebook login process using Selenium for Google Chrome Web Browser
 
-email = input('\nEnter your facebook email')
-password = input('\nEnter your facebook password')
+from selenium import webdriver                      #import library
 
-browser = webdriver.Chrome("C:\\Users\\amann\\Downloads\\driver\\chromedriver")
-browser.get('http://www.facebook.com')
+driver_path = input('\nEnter the path of the chrome driver: ')
+email = input('\nEnter your facebook email: ')
+password = input('\nEnter your facebook password: ')
 
-elem = browser.find_element_by_id('email')
-elem.send_keys(email)
+browser = webdriver.Chrome(driver_path)             #Launches Chrome in automation test mode
+browser.get('http://www.facebook.com')              #Loads facebook website in Chrome
 
-elem = browser.find_element_by_id('pass')
-elem.send_keys(password)
+elem = browser.find_element_by_id('email')          #Finds the email feild using its HTML id
+elem.send_keys(email)                               #Sets the email that the user input to the email textfield
 
-elem = browser.find_element_by_id('loginbutton')
-elem.click()
+elem = browser.find_element_by_id('pass')           #Finds the password feild using its HTML id
+elem.send_keys(password)                            #Sets the password that the user input to the password textfield
+
+elem = browser.find_element_by_id('loginbutton')    #Finds the login button using its HTML id
+elem.click()                                        # Clicks the login button
